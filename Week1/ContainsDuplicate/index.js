2f5739847368ff35eff04ba4387159e04ab92201
huanhust1998 <huanhust1998@gmail.com>
2024-07-25 23:21:13 +0700

    week1:update - completed check contains duplicate

--- Week1/ContainsDuplicate/index.js
+++ Week1/ContainsDuplicate/index.js
@@ -1,5 +1,11 @@
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-function checkContainsDuplicate(nums){
-    
+function checkContainsDuplicate(nums) {
+  const object = {};
+  for (let i = 0; i < nums.length; i++) {
+    const value = object[nums[i]];
+    if (value >= 0) return true;
+    object[nums[i]] = i;
+  }
+  return false;
 }
